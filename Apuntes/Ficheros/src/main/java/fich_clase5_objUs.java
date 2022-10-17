136e2e09473a32ac8ca54008be2e4aea130d17fd
AlumnoM <eduardoflores.9595@gmail.com>
2022-10-17 14:15:51 +0200

    añado clase fich 14.10.22

---
+++ Apuntes/Ficheros/src/main/java/fich_clase5_objUs.java
@@ -0,0 +1,17 @@
+//Fichero de objetod binarios
+//JAVA serializa, convierte la serialización
+//En algo legible
+//import Serializable; (parece que no va)
+//writeObject(objeto)
+//readObject() --> escribe un objeto
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.ObjectInputStream;
+
+public class fich_clase5_objUs {
+    public static void main(String[] args) throws IOException {
+
+
+    }
+    }
